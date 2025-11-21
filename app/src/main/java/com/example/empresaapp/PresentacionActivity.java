03f8c819379b3e108e18bdca713c5c0066bb334b
GitSieRraXSF <jsierra23@udi.edu.co>
2025-11-20 19:06:10 -0500

    Parcial parte 1

--- app/src/main/java/com/example/empresaapp/PresentacionActivity.java
+++ app/src/main/java/com/example/empresaapp/PresentacionActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PresentacionActivity extends AppCompatActivity {
