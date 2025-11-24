23a80ffb37357a391528cced7890e375a3988bb3
GitSieRraXSF <jsierra23@udi.edu.co>
2025-11-24 16:49:02 -0500

    Parcial parte 4 (Completo)

--- app/src/main/java/com/example/empresaapp/CalculoVentaActivity.java
+++ app/src/main/java/com/example/empresaapp/CalculoVentaActivity.java
@@ -1,6 +1,6 @@
 package com.example.empresaapp;
 
-import android.content.Context;
+import  android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +10,6 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CalculoVentaActivity extends AppCompatActivity {
